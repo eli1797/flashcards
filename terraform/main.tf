7e22cdb89ef485ef71b41b63de2ef202bbac0d27
Eli <elibailey97@gmail.com>
2021-07-22 15:12:56 -0600

    failed attempt (so far) at http api gateway terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,8 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_lambda_function" "api-cards" {
-  function_name = "api.cards"
+resource "aws_lambda_function" "api_cards" {
+  function_name = "apiCards-${var.env}"
 
   s3_bucket = "go-code-bucket"
   s3_key    = "${var.env}-code"
@@ -35,8 +35,8 @@ resource "aws_lambda_function" "api-cards" {
   role = aws_iam_role.lambda_exec.arn
 }
 
-resource "aws_cloudwatch_log_group" "api-cards" {
-  name = "/aws/lambda/${aws_lambda_function.api-cards.function_name}"
+resource "aws_cloudwatch_log_group" "api_cards_cw" {
+  name = "/aws/lambda/${aws_lambda_function.api_cards.function_name}"
 
   retention_in_days = 7
 }
@@ -62,3 +62,63 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "cards_gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "gw_integration" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.api_cards.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "gw_route" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "$default"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 7
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_cards.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*/*"
+}
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,4 @@
-# output "lambda_bucket_name" {
-#   description = "Name of the S3 bucket used to store function code."
-
-#   value = aws_s3_bucket.lambda_bucket.id
-# }
+output "function_name" {
+  description = "Name of the Lambda function."
+  value       = aws_lambda_function.api_cards.function_name
+}
